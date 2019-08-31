87f107a4f6235002dc9dda04b2a756c023470746
lifeng <240451928@qq.com>
2019-08-31 10:38:20 +0800

    增加注释

--- src/main/java/the/flash/server/NettyServer.java
+++ src/main/java/the/flash/server/NettyServer.java
@@ -31,6 +31,7 @@ public class NettyServer {
                 .childOption(ChannelOption.SO_KEEPALIVE, true)
                 .childOption(ChannelOption.TCP_NODELAY, true)
                 .childHandler(new ChannelInitializer<NioSocketChannel>() {
+                    @Override
                     protected void initChannel(NioSocketChannel ch) {
                         ch.pipeline().addLast(new Spliter());
                         ch.pipeline().addLast(new PacketDecoder());
